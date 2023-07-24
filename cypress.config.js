51bb3a4dca43ee612c4a73083851414ceb3b0156
NikitaBez <bezmenov.nikita@gmail.com>
2023-07-24 20:32:25 +0900

    upd cypress.config.js - delete project ID

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "deucpr",
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/",
     setupNodeEvents(on, config) {
