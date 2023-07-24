d469c850d480b89d2f9f21f2daa3ab4900235a0b
NikitaBez <bezmenov.nikita@gmail.com>
2023-07-24 21:07:10 +0900

    dashboard ready

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '4nv24c',
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/",
     setupNodeEvents(on, config) {
